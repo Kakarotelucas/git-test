15e8b8060da51c5655810254ed1d2a564cd2e55a
Jialonglu <112746129+Kakarotelucas@users.noreply.github.com>
2022-09-11 00:39:32 +0800

    pull commit

--- src/main/java/com/itheima/GitTest.java
+++ src/main/java/com/itheima/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("Hot-fix Test!");
         System.out.println("push Test!");
+        System.out.println("pull Test!");
     }
 }
